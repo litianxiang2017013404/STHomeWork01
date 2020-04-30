332c8d382142eaf977f0577f571d38ce42c84a12
litianxiang2017013404 <64579921+litianxiang2017013404@users.noreply.github.com>
2020-04-30 20:11:51 +0800

    ...

---
+++ student.java
@@ -0,0 +1,31 @@
+pclass Student {
+    private String name;
+    private double grade;
+    public Student() {
+    }
+    public Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getGrade() {
+        return grade;
+    }
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        stu1.setName("11");
+        stu1.setGrade(99);
+        Student stu2 = new Student("12",88);
+    }
+}
